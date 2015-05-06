67dcf6aa5c69665c6083534cce48e8cf0d34afd0
oveach <oveach@gmail.com>
2015-05-07 01:38:55 +0200

    Add username feature, first iteration, js only

--- app.js
+++ app.js
@@ -5,22 +5,53 @@ $("document").ready(function(){
     // we're using an apache proxy to pass it to port 9090 without the need to expose the service
     var connection = new autobahn.Connection({url: 'ws://' + ROUTER_URL, realm: 'realm1'});
 
+    // will hold the username during the chat session
+    var username = null;
+
     connection.onopen = function(session) {
+        // add status info
+        $("#result").append("Websocket connection established succesfully :)\n\n");
+
+        // show new user window
+        $("div#user-modal").modal();
+        $("div#user-modal").on("shown.bs.modal", function(e){
+            $("input#username").focus();
+
+            $("form#user").submit(function(e){
+                e.preventDefault();
+                if ($("input#username").val().length > 0) {
+                    username = $("input#username").val();
+                    $("div#user-modal").modal("hide");
+                    $("input#message").focus();
+                    session.publish("com.app.user.join", [username], {}, {exclude_me: false});
+                }
+            });
+        });
+
+        session.subscribe("com.app.user.join", function(args){
+            $("#result").append("--- " + args[0] + " joined the chat ---\n");
+            $("#users").append(args[0] + "\n");
+        });
+
+        // activate form
         $("input#message").prop("disabled", false);
         $("#btnSend").prop("disabled", false);
         $("input#message").focus();
-        $("#result").append("Websocket connection established succesfully :)\n\n");
 
+        // display chat message
         session.subscribe("com.app.message", function(args){
             $("#result").append(args[0] + "\n");
         });
 
+        // send chat message
         $("form#formMessage").submit(function(e){
             e.preventDefault();   // to handle submission all in js, with no http request
 
-            // construct and send event
-            var msg = "(session #" + session.id + ") " + $("input#message").val();
-            session.publish("com.app.message", [msg], {}, {exclude_me: false});
+            // construct and send event if message not empty
+            if (username && $("input#message").val().length > 0) {
+                var msg = username + "> " + $("input#message").val();
+                session.publish("com.app.message", [msg], {}, {exclude_me: false});
+            }
 
             // clear input after submission, to let the user type another message easily
             $("input#message").val(null);
